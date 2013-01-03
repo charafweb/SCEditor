7ede90be41c0ca9d49ff9848e36025509062e9f5
charafweb <charafweb@gmail.com>
2013-01-03 19:13:34 +0000

    Update languages/fr.js
    Hi Sam,
    I think this is what you ask me to do in your comment in the pull request #64(I'm not pretty sure, 
    I'm totally new to github, these are my first steps on it, and my english is poor).
    
    Thank you very much

--- languages/fr.js
+++ languages/fr.js
@@ -1,4 +1,3 @@
-
 // add locale:'fr', to your config options.
 
 (function ($) {
@@ -46,7 +45,7 @@
 		"Insert a link": "Insérer un Lien",
 		"Unlink": "Supprimer un Lien",
 		"More": "Plus",
-		"Insert an emoticon": "Insérer une Émoticône",
+		"Insert an emoticon": "Insérer une ةmoticône",
 		"Video URL:": "URL Vidéo",
 		"Insert": "Insérer",
 		"Insert a YouTube video": "Insérer une Vidéo YouTube",
@@ -54,6 +53,15 @@
 		"Insert current time": "Insérer l'heure actuelle",
 		"Print": "Imprimer",
 		"View source": "Afficher le Code source",
+		"Description (optional):": "Description (Optionelle)",
+		"Enter the image URL:": "Entrez l'URL d'image:",
+		"Enter the e-mail address:": "Entrez l'adresse e-mail:",
+		"Enter the displayed text:": "Entrez le texte affiché:",
+		"Enter URL:": "Entrer une URL:",
+		"Enter the YouTube video URL or ID:": "Entrez l'URL vidéo YouTube ou ID:",
+		"Invalid YouTube video": "Invalid vidéo YouTube",
+		"Right-to-Left": "De droite à gauche",
+		"Left-to-Right": "De gauche à droite",
 		dateFormat: "day.month.year"
 	}
 })(jQuery);
